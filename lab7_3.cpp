1450d5aeed1c2a71186cbf5cb7b838a07b82a02f
fordsaranpong <forddbkzero@gmail.com>
2021-01-04 23:43:13 +0700

    lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,33 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+int age;
+cout<<"Enter your age: ";
+cin>>age;
+string r;
+int high,money;
+if(age<=20){
+	cout<<"Enter your height: ";
+	cin>>high;
+		if(high<160){r="UNFRIEND";}
+		else if(high>=160&&high<175){r="FRIEND";}
+		else{
+		cout<<"Enter your property: ";
+		cin>>money;
+				if(money>69000000){r="MARRIED";}
+				else{r="ONE-NIGHT-STAND";}
+			}
+  		  }
+else if(age>20&&age<30){
+ 	cout<<"Enter your property: ";
+		cin>>money;
+				if(money>10000000){r="BEST FRIEND";}
+				else{r="UNFRIEND";}
+			}
+else{r="UNFRIEND";}	
+cout<<"Your status = "<<r;
+	return 0;
+}
